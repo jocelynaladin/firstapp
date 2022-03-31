e384d9249101e7c9c626d8e56074a70f1b500e77
Jocelyn Aladin <aladinjocelyn@gmail.com>
2022-03-30 21:30:52 -0400

    third commit

--- Main.go
+++ Main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-var i int = 42
+const (
+	_  = iota + 5
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+)
 
 func main() {
 
-	fmt.Printf("%v, %T\n", i, i)
-	j := strconv.Itoa(i)
-	fmt.Printf("%v, %T\n", j, j)
+	// fmt.Printf("%v, %T\n", i, i)
+	// j := strconv.Itoa(i)
+	// fmt.Printf("%v, %T\n", j, j)
+	// var r rune = 'a'
+	// fmt.Printf("%v, %T\n", r, r)
+	// var specialistType int
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB\n", fileSize/GB)
 }
