aba2708c9196175f26ba6466e609f340b7fba516
Jocelyn Aladin <aladinjocelyn@gmail.com>
2022-04-06 12:51:50 -0400

    test go map

--- Main.go
+++ Main.go
@@ -1,26 +1,51 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const (
-	_  = iota + 5
-	KB = 1 << (10 * iota)
-	MB
-	GB
-	TB
-	PB
-)
+// const (
+// 	_  = iota + 5
+// 	KB = 1 << (10 * iota)
+// 	MB
+// 	GB
+// 	TB
+// 	PB
+// )
+
+// type Doctor struct {
+// 	number     int
+// 	actorName  string
+// 	companions []string
+// }
+
+type Animal struct {
+	Name   string
+	origin string
+}
+
+type Bird struct {
+	Animal
+	SpeedKPH float32
+	CanFly   bool
+}
 
 func main() {
+	// fileSize := 4000000000.
+	// fmt.Printf("%.2fGB\n", fileSize/GB)
+	// aDoctor := Doctor{
+	// 	number:    3,
+	// 	actorName: "Jocelyn Aladin",
+	// 	companions: []string{
+	// 		"Nadine Aladin",
+	// 		"Philippe Henry",
+	// 		"Natt Aladin",
+	// 	},
+	// }
+	// fmt.Println(aDoctor)
+	b := Bird{}
+	b.Name = "Emu"
+	b.origin = "Australia"
+	b.SpeedKPH = 48
+	b.CanFly = false
 
-	// fmt.Printf("%v, %T\n", i, i)
-	// j := strconv.Itoa(i)
-	// fmt.Printf("%v, %T\n", j, j)
-	// var r rune = 'a'
-	// fmt.Printf("%v, %T\n", r, r)
-	// var specialistType int
-	fileSize := 4000000000.
-	fmt.Printf("%.2fGB\n", fileSize/GB)
+	fmt.Println(b)
 }
